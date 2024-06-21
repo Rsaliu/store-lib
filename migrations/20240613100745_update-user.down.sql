df5400cf4cc9dfb2625a4e63d08c54508f03a40e
RSaliu <ridwanriley@gmail.com>
2024-06-21 11:01:06 +0100

    remove secrets

--- migrations/20240613100745_update-user.down.sql
+++
@@ -1,6 +0,0 @@
--- Add down migration script here
-ALTER TABLE users
-DROP COLUMN email,
-DROP COLUMN confirmed,
-DROP COLUMN created_at,
-DROP COLUMN updated_at;
--- migrations/20240613100745_update-user.up.sql
+++
@@ -1,6 +0,0 @@
--- Add up migration script here
-ALTER TABLE users
-ADD COLUMN email TEXT NOT NULL DEFAULT 'noemail@yahoo.com',
-ADD COLUMN confirmed BOOLEAN NOT NULL DEFAULT FALSE,
-ADD COLUMN created_at TIMESTAMP not null default now(),
-ADD COLUMN updated_at TIMESTAMP not null default now();
